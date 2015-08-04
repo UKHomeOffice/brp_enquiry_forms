c9cf3b417091331c4e0a1dbe24208b195a21eaf3
JoeChapman <joseph.philip.chapman@gmail.com>
2015-08-04 10:13:08 +0100

    Update 'lost-or-stolen' journey content
    - minor copy changes
    - add lost-or-stolen folder
    - move/copy apt files to new folder
    - make template definitions explicit in routes

--- routes/steps/lost.js
+++ routes/steps/lost.js
@@ -7,6 +7,7 @@ module.exports = {
     next: '/inside-uk'
   },
   '/inside-uk': {
+    template: 'lost-or-stolen/inside-uk.html',
     fields: [
       'inside-uk',
       'country'
@@ -15,6 +16,7 @@ module.exports = {
   },
   '/date-lost': {
     controller: require('../../controllers/date-lost'),
+    template: 'lost-or-stolen/date-lost.html',
     fields: [
       'date-lost',
       'date-lost-day',
@@ -26,7 +28,7 @@ module.exports = {
   },
   '/personal-details': {
     controller: require('../../controllers/personal-details'),
-    template: 'personal-details-brp.html',
+    template: 'lost-or-stolen/personal-details.html',
     fields: [
       'fullname',
       'date-of-birth',
@@ -41,6 +43,7 @@ module.exports = {
   },
   '/contact-details': {
     controller: require('../../controllers/contact-details'),
+    template: 'lost-or-stolen/contact-details.html',
     fields: [
       'email',
       'no-email',
@@ -55,6 +58,7 @@ module.exports = {
   },
   '/check-details': {
     controller: require('../../controllers/check-details'),
+    template: 'lost-or-stolen/check-details.html',
     fields: [
       'org-help',
       'rep-name',
@@ -65,6 +69,7 @@ module.exports = {
   },
   '/confirmation': {
     controller: require('../../controllers/confirmation'),
+    template: 'lost-or-stolen/confirmation.html',
     backLink: false,
     next: '/done'
   },
