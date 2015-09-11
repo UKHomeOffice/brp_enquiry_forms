7459bc4e346026092e349c860ca40c530f0b88c2
JoeChapman <joseph.philip.chapman@gmail.com>
2015-09-11 10:28:29 +0100

    DSP-177: /collection/reasons
    - add collection/reasons page
    - add reasons fileds, step def, template and translations

--- routes/fields.js
+++ routes/fields.js
@@ -420,6 +420,77 @@ module.exports = {
       value: 'true'
     }
   },
+  'reason-radio': {
+    validate: ['required'],
+    options: [{
+      value: 'under-age',
+      label: 'fields.reason-radio.under-age.label',
+      toggle: 'reason-under-age-group',
+    }, {
+      value: 'non-identity',
+      label: 'fields.reason-radio.non-identity.label',
+      toggle: 'reason-non-identity-group',
+    }, {
+      value: 'others-identty',
+      label: 'fields.reason-radio.others-identity.label',
+      toggle: 'reason-others-identity-group',
+    }, {
+      value: 'others-auth',
+      label: 'fields.reason-radio.others-auth.label',
+      toggle: 'reason-others-auth-group',
+    }, {
+      value: 'passport-family',
+      label: 'fields.reason-radio.reason-passport-family.label',
+      toggle: 'reason-reason-passport-family-group',
+    },
+    {
+      value: 'passport-lost',
+      label: 'fields.reason-radio.reason-passport-lost.label',
+      toggle: 'reason-reason-passport-lost-group',
+    },
+    {
+      value: 'passport-stamp',
+      label: 'fields.reason-radio.reason-passport-stamp.label',
+      toggle: 'reason-reason-passport-stamp-group',
+    },
+    {
+      value: 'no-brp',
+      label: 'fields.reason-radio.reason-no-brp.label',
+      toggle: 'reason-reason-no-brp-group',
+    },
+    {
+      value: 'other',
+      label: 'fields.reason-radio.reason-other.label',
+      toggle: 'reason-reason-other-group',
+    }]
+  },
+  'reason-under-age': {
+    label: 'fields.reason-under-age.label'
+  },
+  'reason-non-identity': {
+    label: 'fields.reason-non-identity.label'
+  },
+  'reason-others-identity': {
+    label: 'fields.reason-others-identity.label'
+  },
+  'reason-others-auth': {
+    label: 'fields.reason-others-auth.label'
+  },
+  'reason-passport-family': {
+    label: 'fields.reason-passport-family.label'
+  },
+  'reason-passport-lost': {
+    label: 'fields.reason-passport-lost.label'
+  },
+  'reason-passport-stamp': {
+    label: 'fields.reason-passport-stamp.label'
+  },
+  'reason-no-brp': {
+    label: 'fields.reason-no-brp.label'
+  },
+  'reason-other': {
+    label: 'fields.reason-other.label'
+  },
   'truncated': {
     validate: ['required'],
     className: 'inline',
