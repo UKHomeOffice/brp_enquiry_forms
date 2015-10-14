3d34cbe406315cd7c31a84e3c4ece384e869182f
JoeChapman <joseph.philip.chapman@gmail.com>
2015-10-14 17:13:23 +0100

    DSP-211: tidy up folders and files after moving all form journeys to apps/
    - remove controllers
    - move/update common fields to apps/common/fields
    - separate those common fields
    - fix acceptance tests and amend correct-mistakes html

--- apps/correct-mistakes/fields/index.js
+++ apps/correct-mistakes/fields/index.js
@@ -5,5 +5,6 @@ var _ = require('underscore');
 module.exports = _.extend(
   require('./about-error'),
   require('./truncated'),
-  require('./uk-address')
+  require('./uk-address'),
+  require('./location-applied')
 );
