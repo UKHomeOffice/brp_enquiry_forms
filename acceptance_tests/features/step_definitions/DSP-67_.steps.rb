38d5b974da7b7b65dfff69186893411024ae9298
Ben Marvell <madmarv187@gmail.com>
2015-10-16 11:17:42 +0100

    Update tests to look for "Send" and not "send"

--- acceptance_tests/features/step_definitions/DSP-67_.steps.rb
+++ acceptance_tests/features/step_definitions/DSP-67_.steps.rb
@@ -1,15 +1,15 @@
-When(/^I check the I do not have an email address checkbox$/) do             
+When(/^I check the I do not have an email address checkbox$/) do
   check('no-email')
-end                                                                          
-                                                                             
+end
+
 When(/^I enter an email address without an @$/) do
   fill_in('email', :with => 'btsbullerproof.cr')
-end                                                                          
-                                                                             
+end
+
 When(/^I fill the Phone number field with special characters$/) do
   fill_in('phone', :with => '^&^&^&^&')
-end                                                                          
-                                                                             
+end
+
 When(/^I fill the Phone number field with text$/) do
   fill_in('phone', :with => 'vghvmvmg')
 end
