74b5a8bf46fe1bcfb67eb5531e64341e8ddc9179
Ben Marvell <madmarv187@gmail.com>
2015-10-19 10:40:20 +0100

    Removed buttons fields.
    These are not needed. They are autonamespaced in the template mixins.
    
    Updated references to buttons.send in the templates

--- apps/common/fields/index.js
+++ apps/common/fields/index.js
@@ -3,7 +3,6 @@
 var _ = require('underscore');
 
 module.exports = _.extend(
-  require('./buttons'),
   require('./contact-details'),
   require('./personal-details'),
   require('./organisation-details')
