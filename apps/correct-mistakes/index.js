4131649b724318728557d351f1a2a4c555c86d6a
JoeChapman <joseph.philip.chapman@gmail.com>
2015-10-27 10:58:09 +0000

    use hof in correct mistakes

--- apps/correct-mistakes/index.js
+++ apps/correct-mistakes/index.js
@@ -1,12 +1,15 @@
 'use strict';
 
+var hof = require('hof');
+var wizard = hof.wizard;
+var mixins = hof.mixins;
+var i18nFuture = hof.i18n;
 var router = require('express').Router();
-var wizard = require('hmpo-form-wizard');
-var mixins = require('hmpo-template-mixins');
-var _ = require('underscore');
 var path = require('path');
+var _ = require('underscore');
+
 var fields = _.extend({}, require('../common/fields/'), require('./fields/'));
-var i18n = require('i18n-future')({
+var i18n = i18nFuture({
   path: path.resolve(__dirname, './translations/__lng__/__ns__.json')
 });
 
