0a109aa3bd08d4124564ba99e1d0287a7841965c
Ben Marvell <madmarv187@gmail.com>
2015-11-18 09:03:45 +0000

    Toggle a reason message if user selects no in truncation page.

--- apps/correct-mistakes/fields/truncated.js
+++ apps/correct-mistakes/fields/truncated.js
@@ -4,12 +4,17 @@ module.exports = {
   'truncated': {
     validate: ['required'],
     className: 'inline',
+    legend: {
+      value: '',
+      className: 'visuallyhidden'
+    },
     options: [{
       value: 'yes',
       label: 'fields.truncated.options.yes.label'
     }, {
       value: 'no',
-      label: 'fields.truncated.options.no.label'
+      label: 'fields.truncated.options.no.label',
+      toggle: 'truncated-reason-no'
     }]
   },
   'truncation-page': {
