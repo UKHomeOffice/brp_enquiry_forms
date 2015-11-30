f6dddf2e5783bdc64a3f72ff525d398c8ba4a5d1
Daniel A.C. Martin <github@daniel-martin.co.uk>
2015-11-30 13:53:16 +0000

    [BRP-72] In country - Lost/Stolen - Step 4 (lost overseas) - Make email address compulsory

--- apps/lost-stolen-damaged/steps.js
+++ apps/lost-stolen-damaged/steps.js
@@ -38,6 +38,7 @@ module.exports = {
     next: '/contact-details'
   },
   '/contact-details': {
+    controller: require('./controllers/contact-details'),
     fields: [
       'email',
       'no-email',
