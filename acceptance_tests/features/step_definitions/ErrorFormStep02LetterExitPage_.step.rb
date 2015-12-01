356d39a55676ed9980544f209a94419ef3ccf034
Daniel A.C. Martin <github@daniel-martin.co.uk>
2015-12-01 14:54:31 +0000

    [BRP-70] Fix inconsistent text in test

--- acceptance_tests/features/step_definitions/ErrorFormStep02LetterExitPage_.step.rb
+++ acceptance_tests/features/step_definitions/ErrorFormStep02LetterExitPage_.step.rb
@@ -8,7 +8,7 @@ end
 
 Then(/^I am on the Enrolment letter exit page$/) do
   page.should have_content('Enrolment letter problems')
-  page.should have_content('As you have not received a BRP yet and have a problem with your enrolment, please follow our Enrolment letter process to enquiry about your problem.')
+  page.should have_content('As you have not received a BRP yet and have a problem with your enrolment, please follow our Enrolment letter process to enquire about your problem.')
   find_link('Close').visible?
   # FIXME: Edit and uncomment once we have something to link to
   #find_link('Enrolment letter')
