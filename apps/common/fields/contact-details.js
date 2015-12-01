4b843a897938bdc7ef44c67451e1b30ca69d687e
Tim Gent <tim.gent@gmail.com>
2015-12-01 08:33:33 +0000

    Updated common fields and translations - Added house number field - Pointed all translations to common translations as these labels and validation messages will be the same

--- apps/common/fields/contact-details.js
+++ apps/common/fields/contact-details.js
@@ -18,9 +18,17 @@ module.exports = {
   phone: {
     label: 'fields.phone.label'
   },
+  'contact-address-house-number': {
+    validate: ['required'],
+      label: 'common-fields.address-house-number.label',
+      dependent: {
+        value: 'true',
+        field: 'no-email'
+      }
+  },
   'contact-address-street': {
     validate: ['required'],
-    label: 'fields.address-street.label',
+    label: 'common-fields.address-street.label',
     dependent: {
       value: 'true',
       field: 'no-email'
@@ -28,18 +36,18 @@ module.exports = {
   },
   'contact-address-town': {
     validate: ['required'],
-    label: 'fields.address-town.label',
+    label: 'common-fields.address-town.label',
     dependent: {
       value: 'true',
       field: 'no-email'
     }
   },
   'contact-address-county': {
-    label: 'fields.address-county.label',
+    label: 'common-fields.address-county.label',
   },
   'contact-address-postcode': {
     validate: ['required'],
-    label: 'fields.address-postcode.label',
+    label: 'common-fields.address-postcode.label',
     dependent: {
       value: 'true',
       field: 'no-email'
