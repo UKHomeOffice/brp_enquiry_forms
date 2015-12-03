7a205715ad973b164e5f74776691c230f8775cb6
Ben Marvell <madmarv187@gmail.com>
2015-12-03 12:36:49 +0000

    Enable typeahead validation on the server side.
    Using the options key in fields you can validate if the data entered matches any value in options.
      - If you add an empty string to the options the template-mixins will render an empty option row with no value
      - this is used to ensure that the first item in the select list is not pre populated in the typeahead
    
    Adding typeahead class triggers the frontend typeahead to appear.

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,34 +14,44 @@ var Bloodhound = require('typeahead.js-browserify').Bloodhound;
 
 typeahead.loadjQueryPlugin();
 
-var nonEuCountries = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.whitespace,
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  local: require('../countries').nonEuCountries
-});
+$('.typeahead').each(function applyTypeahead() {
+  var $el = $(this);
+  var $parent = $el.parent();
+  var attributes = $el.prop('attributes');
+  var $input = $('<input/>');
+  var selectedValue = $el.val();
+  var typeaheadList = $el.find('option').map(function mapOptions() {
+    if (this.value === '') {
+      // remove any empty values from typeahead
+      /*eslint consistent-return: 0*/
+      return;
+      /*eslint consistent-return: 1*/
+    }
+    return this.value;
+  }).get();
 
-var allCountries = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.whitespace,
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  local: require('../countries').allCountries
-});
+  // remove the selectbox
+  $el.remove();
 
+  $.each(attributes, function applyAttributes() {
+    $input.attr(this.name, this.value);
+  });
 
-$('#nationality, #nationality-error, #nominated-nationality')
-.typeahead({
-  minLength: 1,
-  hint: false,
-  limit: 5
-}, {
-  name: 'nonEuCountries',
-  source: nonEuCountries
-});
+  $input.removeClass('js-hidden');
+  $input.addClass('form-control');
+  $input.val(selectedValue);
 
-$('#country, #someone-else-nationality, #change-person-nationality').typeahead({
-  minLength: 1,
-  hint: false,
-  limit: 5
-}, {
-  name: 'allCountries',
-  source: allCountries
+  $parent.append($input);
+
+  $input.typeahead({
+    minLength: 1,
+    hint: false,
+    limit: 5
+  }, {
+    source: new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.whitespace,
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      local: typeaheadList
+    })
+  });
 });
