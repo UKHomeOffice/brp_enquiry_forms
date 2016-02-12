42890eaaad3cafe4e5218327dbd5545044b6622a
Ben Marvell <madmarv187@gmail.com>
2016-02-12 11:44:58 +0000

    Update text for lost-letter page.
    This gives the user more than one option of contact instead of just a phone number.

--- acceptance_tests/features/step_definitions/DeliveryFormStep01NoLetterExitPage.rb
+++ acceptance_tests/features/step_definitions/DeliveryFormStep01NoLetterExitPage.rb
@@ -1,6 +1,6 @@
 Then(/^I am on the Delivery No Letter exit page$/) do
   page.should have_content('Contact us')
-  page.should have_content('We need to know the date on your letter so we can look into why you haven\'t received your BRP. To find out when your letter was sent, call customer services on 0300 123 2241.')
+  page.should have_content('If you haven\'t yet received a letter, the Home Office may still be processing your application.')
   find_link('Close').visible?
   delete_cookie('hmbrp.sid')
 end
