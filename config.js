3d1c5f065486a833f33279895e1f01f441a4796b
Ben Marvell <madmarv187@gmail.com>
2016-03-07 13:55:10 +0000

    Add GA tag from GA_TAG variable.

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ module.exports = {
   env: process.env.NODE_ENV,
   port: process.env.PORT || 8080,
   listen_host: process.env.LISTEN_HOST || '0.0.0.0',
+  gaTag: process.env.GA_TAG || false,
   session: {
     secret: process.env.SESSION_SECRET || 'howdoesyourgardengrow',
     ttl: process.env.SESSION_TTL || 1800 /* 30 mins timeout */
