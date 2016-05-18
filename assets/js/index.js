3e036cc8884ec6255302634d23759015d587c51b
Ben Marvell <madmarv187@gmail.com>
2016-05-18 13:17:29 +0100

    Sort matches to start of word first

--- assets/js/index.js
+++ assets/js/index.js
@@ -49,7 +49,29 @@ $('.typeahead').each(function applyTypeahead() {
     source: new Bloodhound({
       datumTokenizer: Bloodhound.tokenizers.whitespace,
       queryTokenizer: Bloodhound.tokenizers.whitespace,
-      local: typeaheadList
+      local: typeaheadList,
+      sorter: function sorter(a, b) {
+        var input = $input.val();
+        var startsWithInput = function startsWithInput(x) {
+          return (x.toLowerCase().substr(0, input.length) === input.toLowerCase()) ? -1 : 1;
+        };
+
+        var compareAlpha = function compareAlpha(x, y) {
+          var less = (x < y) ? -1 : 1;
+          return (x === y) ? 0 : less;
+        };
+
+        var compareStartsWithInput = function compareStartsWithInput(x, y) {
+          var startsWithFirst = startsWithInput(x);
+          var startsWithSecond = startsWithInput(y);
+
+          return (startsWithFirst === startsWithSecond) ? 0 : startsWithFirst;
+        };
+
+        var first = compareStartsWithInput(a, b);
+
+        return (first === 0) ? compareAlpha(a, b) : first;
+      }
     }),
     limit: 100
   });
