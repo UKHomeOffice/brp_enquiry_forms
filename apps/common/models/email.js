87a3c75bab6c96d4d25fb813a5d9a4fab4e2c8db
Leonard Martin <leonard.martin@gmail.com>
2017-06-30 16:26:14 +0100

    Clean up email model to use ES6 classes

--- apps/common/models/email.js
+++ apps/common/models/email.js
@@ -1,24 +1,19 @@
 'use strict';
 
-var util = require('util');
 var emailService = require('../../../services/email');
 var Model = require('hof-model');
 var _ = require('underscore');
 
-function EmailModel() {
-  Model.apply(this, arguments);
+module.exports = class Email extends Model {
+
+  save(callback) {
+    // we omit keys that are not part of the session data
+    emailService.send({
+      template: this.get('template'),
+      to: this.get('email'),
+      subject: this.get('subject'),
+      dataToSend: _.omit(this.toJSON(), ['steps', 'csrf-secret', 'template'])
+    }, callback);
+  };
+
 }
-
-util.inherits(EmailModel, Model);
-
-EmailModel.prototype.save = function save(callback) {
-  // we omit keys that are not part of the session data
-  emailService.send({
-    template: this.get('template'),
-    to: this.get('email'),
-    subject: this.get('subject'),
-    dataToSend: _.omit(this.toJSON(), ['steps', 'csrf-secret', 'template'])
-  }, callback);
-};
-
-module.exports = EmailModel;
