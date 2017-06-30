e5c5d2b0f232f07f7b2e9524f6caf4121988067c
Leonard Martin <leonard.martin@gmail.com>
2017-06-30 16:00:32 +0100

    Use hof-theme for client-side js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,6 @@
 'use strict';
 
-var toolkit = require('hof').toolkit;
-var helpers = toolkit.helpers;
-var progressiveReveal = toolkit.progressiveReveal;
-var formFocus = toolkit.formFocus;
-
-helpers.documentReady(progressiveReveal);
-helpers.documentReady(formFocus);
+require('hof-theme-govuk');
 
 var $ = require('jquery');
 var typeahead = require('typeahead-aria');
