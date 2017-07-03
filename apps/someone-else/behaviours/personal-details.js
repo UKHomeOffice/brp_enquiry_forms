3f1515ef162f307ead20e647629136e9d257793a
Leonard Martin <leonard.martin@gmail.com>
2017-07-03 12:44:50 +0100

    Standardise "over 18" validation logic
    Use the same logic for each case where we check the date of birth against an earlier asnwer of "I am under 18"

--- apps/someone-else/behaviours/personal-details.js
+++ apps/someone-else/behaviours/personal-details.js
@@ -1,24 +1,16 @@
 'use strict';
 
-const moment = require('moment');
-
 module.exports = superclass => class PersonalDetails extends superclass {
 
-  validate(req, res, next) {
-
-    const declaredUnder18 = req.sessionModel.get('someone-else-reason-radio') === 'under-age';
-    const isOver18 = moment(req.form.values['date-of-birth']).isBefore(moment().subtract({years: 18}));
-
-    if (declaredUnder18 && isOver18) {
-      const error = new this.ValidationError('date-of-birth', {
-        type: 'under-18'
-      });
-      return next({
-        'date-of-birth': error
+  configure(req, res, next) {
+    if (req.sessionModel.get('someone-else-reason-radio') === 'under-age') {
+      req.form.options.fields['date-of-birth'].validate = req.form.options.fields['date-of-birth'].validate || [];
+      req.form.options.fields['date-of-birth'].validate.push({
+        type: 'after',
+        arguments: [18, 'years']
       });
     }
-
-    super.validate(req, res, next);
+    next();
   }
 
 };
