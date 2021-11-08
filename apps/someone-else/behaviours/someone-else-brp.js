8b99681e2e825406541b3e7dbba02cc1aa345e81
Hugo Jeffreys <hugo.jeffreys@digital.homeoffice.gov.uk>
2021-11-08 11:19:58 +0000

    Correct regex brp validation on someone-else behaviour.

--- apps/someone-else/behaviours/someone-else-brp.js
+++ apps/someone-else/behaviours/someone-else-brp.js
@@ -4,8 +4,8 @@
 module.exports = (superclass) =>
   class BrpValidator extends superclass {
     saveValues(req, res, next) {
-        // TODO = USE .MATCH TO MATCH REGEX. SEE STRING.PROTOTYPE.MATCH() ON MOZILLA
-      if (req.form.values['someone-else-id-type'] === 'brp-card' && req.form.values['someone-else-id-number'] != /^[a-z][a-z](\d|X)\d{6}$/gi ) {
+      const brpRegex = new RegExp(/^[a-z][a-z](\d|X)\d{6}$/i);
+      if (req.form.values['someone-else-id-type'] === 'brp-card' && brpRegex.test(req.form.values['someone-else-id-number']) !== true) {
         return next({
           'someone-else-id-number': new this.ValidationError('someone-else-id-number', {
             type: "brp-valid",
