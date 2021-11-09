a9cb8b97baf8c30e2f7ef459e7ff58b5e908c475
Hugo Jeffreys <hugo.jeffreys@digital.homeoffice.gov.uk>
2021-11-09 11:35:21 +0000

    Add corrections to linting errors.

--- apps/someone-else/behaviours/someone-else-brp.js
+++ apps/someone-else/behaviours/someone-else-brp.js
@@ -1,18 +1,23 @@
-"use strict";
+'use strict';
 
 
-module.exports = (superclass) =>
+module.exports = superclass =>
   class BrpValidator extends superclass {
-    saveValues(req, res, next) {
+    validate(req, res, next) {
       const brpRegex = new RegExp(/^[a-z][a-z](\d|X)\d{6}$/i);
-      if (req.form.values['someone-else-id-type'] === 'brp-card' && brpRegex.test(req.form.values['someone-else-id-number']) !== true) {
+      if (
+        req.form.values['someone-else-id-type'] === 'brp-card' &&
+        brpRegex.test(req.form.values['someone-else-id-number']) !== true
+      ) {
         return next({
-          'someone-else-id-number': new this.ValidationError('someone-else-id-number', {
-            type: "brp-valid",
-          }),
+          'someone-else-id-number': new this.ValidationError(
+            'someone-else-id-number',
+            {
+              type: 'brp-valid'
+            }
+          )
         });
-      } else {
-        super.saveValues(req, res, next);
-      }
+      } super.validate(req, res, next);
+      return next;
     }
   };
