2ee447d3c173d5dea839446d08fdab616c963e93
Hugo Jeffreys <hugo.jeffreys@digital.homeoffice.gov.uk>
2022-01-11 13:14:55 +0000

    Alter change-path redirect behaviour from hard coded live url to relative path.

--- apps/not-arrived/behaviours/change-path.js
+++ apps/not-arrived/behaviours/change-path.js
@@ -4,8 +4,7 @@ module.exports = superclass =>
   class LetterRecieved extends superclass {
     saveValues(req, res, next) {
       if(req.form.values.collection === 'yes') {
-        res.statusCode = 302;
-        res.setHeader('Location', 'https://www.biometric-residence-permit.service.gov.uk/collection/where');
+        res.redirect(302, `${req.headers.origin}/collection/where`);
         res.end();
       }else{
         super.saveValues(req, res, next);
