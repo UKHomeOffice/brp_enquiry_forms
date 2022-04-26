75fea3f7293ed4e26a66827cde14f13e70734671
Rhodine Orleans-Lindsay <Rhodine.Orleans-Lindsay@digital.homeoffice.gov.uk>
2022-04-26 13:11:13 +0100

    add question on application type to lost-stolen journey

---
+++ apps/lost-stolen/fields/application-type.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+    'application-type-other': {
+        validate: ['required'],
+        dependent: {
+          field: 'application-type',
+          value: 'Other'
+        }
+      }
+};
