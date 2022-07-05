4a33f967d8b2e498d44a66c671b1d995c8481c23
Rhyad Zergane <rhyad.zergane@digital.homeoffice.gov.uk>
2022-07-05 09:27:12 +0100

    fix linting in list_uris.js

--- test/_zap/list_uris.js
+++ test/_zap/list_uris.js
@@ -4,21 +4,25 @@ const ZAP_HOST = process.env.ZAP_HOST || 'localhost';
 const ZAP_PORT = process.env.ZAP_PORT || 8090;
 
 
-const listURIs = ()=>{
-    request.get(`http://${ZAP_HOST}:${ZAP_PORT}/JSON/core/view/sites/?`,(err,resp,body) =>{
-        if(err){
-            console.log(err);
+const listURIs = () => {
+  request.get(`http://${ZAP_HOST}:${ZAP_PORT}/JSON/core/view/sites/?`, (err, resp, body) => {
+    if(err) {
+      console.log(err);
+    }
+    const siteJson = JSON.parse(body);
+
+    request(`http://${ZAP_HOST}:${ZAP_PORT}/JSON/core/view/urls/?baseurl=${siteJson.sites[0]}`,
+      (siteErr, siteResp, siteBody) => {
+        if(siteErr) {
+          console.log(siteErr);
         }
-        const siteJson = JSON.parse(body);
+        const visitedJson = JSON.parse(siteBody);
 
-        request(`http://${ZAP_HOST}:${ZAP_PORT}/JSON/core/view/urls/?baseurl=${siteJson.sites[0]}`, (err,resp,body) =>{
-            const visitedJson = JSON.parse(body);
-
-            visitedJson.urls.forEach(element => {
-                console.log(element);
-            });
+        visitedJson.urls.forEach(element => {
+          console.log(element);
         });
-    });
-}
+      });
+  });
+};
 
 listURIs();
