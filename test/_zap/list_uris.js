5f3efb72dee7dda96faebbed3767ef372b69cfbd
Rhyad Zergane <rhyad.zergane@digital.homeoffice.gov.uk>
2022-07-05 14:54:07 +0100

    Add spider scan to pipeline

--- test/_zap/list_uris.js
+++ test/_zap/list_uris.js
@@ -1,27 +1,31 @@
-const request = require('request');
+const axios = require('axios').default;
 
 const ZAP_HOST = process.env.ZAP_HOST || 'localhost';
 const ZAP_PORT = process.env.ZAP_PORT || 8090;
+const CONTEXT_NAME = process.env.CONTEXT_NAME || 'test';
 
 
-const listURIs = () => {
-  request.get(`http://${ZAP_HOST}:${ZAP_PORT}/JSON/core/view/sites/?`, (err, resp, body) => {
-    if(err) {
-      console.log(err);
-    }
-    const siteJson = JSON.parse(body);
+const listURIs = async () => {
+  const context = {
+    contextName: CONTEXT_NAME
+  };
+  await axios({url: `http://${ZAP_HOST}:${ZAP_PORT}/JSON/context/action/newContext`,
+    method: 'GET', params: context}).catch(err => console.log(err));
 
-    request(`http://${ZAP_HOST}:${ZAP_PORT}/JSON/core/view/urls/?baseurl=${siteJson.sites[0]}`,
-      (siteErr, siteResp, siteBody) => {
-        if(siteErr) {
-          console.log(siteErr);
-        }
-        const visitedJson = JSON.parse(siteBody);
+  const site = await axios.get(`http://${ZAP_HOST}:${ZAP_PORT}/JSON/core/view/sites/?`).catch(err => console.log(err));
+  const siteJson = site.data;
 
-        visitedJson.urls.forEach(element => {
-          console.log(element);
-        });
-      });
+  const visited = await axios(`http://${ZAP_HOST}:${ZAP_PORT}/JSON/core/view/urls/?baseurl=${siteJson.sites[0]}`)
+    .catch(err => console.log(err));
+
+  const visitedJson = visited.data;
+
+  visitedJson.urls.forEach(async element => {
+    await axios({url: `http://${ZAP_HOST}:${ZAP_PORT}/JSON/context/action/includeInContext/`, method: 'GET',
+      params: {
+        contextName: CONTEXT_NAME,
+        regex: element
+      }}).catch(err => console.log(err));
   });
 };
 
---
+++ test/_zap/run_spider_scan.js
@@ -0,0 +1,48 @@
+const axios = require('axios').default;
+
+const ZAP_HOST = process.env.ZAP_HOST || 'localhost';
+const ZAP_PORT = process.env.ZAP_PORT || 8090;
+const CONTEXT_NAME = process.env.CONTEXT_NAME || 'test';
+
+
+const runSpiderScan = async () => {
+  const formData = {
+    contextName: CONTEXT_NAME,
+    maxChildren: 0,
+    recurse: true,
+    subtreeOnly: true
+  };
+
+  const scanStart = await axios({url: `http://${ZAP_HOST}:${ZAP_PORT}/JSON/spider/action/scan`, method: 'GET',
+    params: formData}).catch(err => console.log(err));
+
+  console.log(scanStart.data);
+  const scanID = scanStart.data.scan;
+
+  console.log(scanID);
+
+  let percentageComplete = 0;
+  let resp = undefined;
+
+  while(percentageComplete < 100) {
+    resp = await axios({url: `http://${ZAP_HOST}:${ZAP_PORT}/JSON/spider/view/status`, method: 'GET',
+      params: formData});
+    console.log(`Spider scan percentage complete: ${percentageComplete}%`);
+    percentageComplete = resp.data.status;
+  }
+
+  // show final 100% completion
+  console.log(`Spider scan percentage complete: ${percentageComplete}%`);
+
+  const scanResults = await axios({url: `http://${ZAP_HOST}:${ZAP_PORT}/JSON/spider/view/fullResults/`,
+    method: 'GET',
+    params: {
+      scanId: scanID
+    }}).catch(err => console.log(err));
+
+  scanResults.data.fullResults[0].urlsInScope.forEach(element => {
+    console.log('url: ' + element.url + ' status code: ' + element.statusCode);
+  });
+};
+
+runSpiderScan();
