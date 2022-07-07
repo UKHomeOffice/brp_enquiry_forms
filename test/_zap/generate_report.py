5b5a9c8c3f061937ef55415db51f1913498908bd
Rhyad Zergane <rhyad.zergane@digital.homeoffice.gov.uk>
2022-07-07 11:01:34 +0100

    fix format of report file name

--- test/_zap/generate_report.py
+++ test/_zap/generate_report.py
@@ -12,6 +12,8 @@ header = {
 ZAP_HOST = os.environ.get('ZAP_HOST') or 'localhost'
 ZAP_PORT = os.environ.get('ZAP_PORT') or 8090
 CONTEXT_NAME = os.environ.get('CONTEXT_NAME') or 'test'
+cwd = os.getcwd()
+print(cwd)
 
 reportParams = {
     'title': 'test report for build pipeline poc',
@@ -38,13 +40,13 @@ reportParams = {
     'includedConfidences': '',
     # i.e. "Informational", "Low", "Medium", and "High". leave empty for all
     'includedRisks': '',
-    'reportFileName': 'test report for drone POC ' +
-    str(date.day) + '/' + str(date.month) + '/' + str(date.year),
+    'reportFileName': 'test-report-for-drone-POC-' +
+    str(date.day) + '-' + str(date.month) + '-' + str(date.year),
     # Not too relevant as we can use date lib in python
     'reportFileNamePattern': '',
-    'reportDir': './',
+    'reportDir': '',
     # Should we display the report after generating
-    'display': False
+    'display': True
 }
 
 try:
