055e879702055cd5b4ec0dc20b2be0b949170253
Martin Crawley <martin.crawley@digital.homeoffice.gov.uk>
2023-04-11 14:54:56 +0100

    Corrected description in unit test

--- test/_unit/apps/common/services/emailService.spec.js
+++ test/_unit/apps/common/services/emailService.spec.js
@@ -4,8 +4,8 @@ const EmailService = require('../../../../../services/email');
 const config = require('../../../../../config');
 
 describe('services/email', () => {
-  describe('.send()', () => {
-    it('calls the send endpoint on the email service with the model data', () => {
+  describe('.getEmailRecipient()', () => {
+    it('suffixes the integration email to the caseworker email if configured', () => {
       config.email['integration-email-recipient'] = 'integration@test.com';
       config.email.caseworker.collection = 'caseworker@test.com';
       const email = EmailService.getEmailRecipient({ template: 'collection' });
