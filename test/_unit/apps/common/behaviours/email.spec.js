dac4c5aad24278c64b9193c7328e35dee52508ed
Martin Crawley <martin.crawley@digital.homeoffice.gov.uk>
2023-04-12 10:20:26 +0100

    Fixed unit test issue

--- test/_unit/apps/common/behaviours/email.spec.js
+++ test/_unit/apps/common/behaviours/email.spec.js
@@ -111,7 +111,7 @@ describe('apps/common/controllers/confirm', () => {
       controller.saveValues(req, res, err => {
         expect(err).not.to.be.ok;
         setStub.should.have.been.calledWith('subject',
-          'Form submitted: Report a problem with your new BRP (test-error)');
+          'Form submitted: Report a problem with your new BRP (test-error). Ref: fpgyxSgw7');
       });
     });
 
