4c078541ad57e0c6aaaecaf0d46aba64db0d8d8a
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2024-05-03 13:22:01 +0100

    update someone-else journey and amend other journeys

--- apps/someone-else/sections/summary-data-sections.js
+++ apps/someone-else/sections/summary-data-sections.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const moment = require('moment');
+const PRETTY_DATE_FORMAT = 'D MMMM YYYY';
+
 module.exports = {
   'nominee-information': {
     steps: [
@@ -30,6 +33,49 @@ module.exports = {
       {
         step: '/arrange',
         field: 'someone-else-id-number'
+      }
+    ]
+  },
+  'personal-details': {
+    steps: [
+      {
+        step: '/personal-details',
+        field: 'fullname'
+      },
+      {
+        step: '/personal-details',
+        field: 'date-of-birth',
+        parse: d => d && moment(d).format(PRETTY_DATE_FORMAT)
+      },
+      {
+        step: '/personal-details',
+        field: 'nationality'
+      },
+      {
+        step: '/contact-details',
+        field: 'use-address',
+        parse: (list, req) => {
+          if (!req.sessionModel.get('use-address')) {
+            return null;
+          }
+          return `${req.sessionModel.get('contact-address-house-number')} \n` +
+            `${req.sessionModel.get('contact-address-street')} \n` +
+            `${req.sessionModel.get('contact-address-town')} \n` +
+            `${req.sessionModel.get('contact-address-county')} \n` +
+            `${req.sessionModel.get('contact-address-postcode')}`;
+        }
+      },
+      {
+        step: '/personal-details',
+        field: 'passport'
+      },
+      {
+        step: '/contact-details',
+        field: 'email'
+      },
+      {
+        step: '/contact-details',
+        field: 'phone'
       }
     ]
   }
